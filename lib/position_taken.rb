5ccf070526d095290bef9198367824a416a3f602
ANTONIO J ABRAHAM <tony.calablava@gmail.com>
2019-06-09 03:41:06 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,11 +2,4 @@ def #position_taken?
  if array[0] = "  "
  puts "false"
 end
-
-def some_new_fabulous_method
-    if position_taken?
-        execute something
-    else
-        execute something else
-    end
 end
